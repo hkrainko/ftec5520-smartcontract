f8ddf5476a4fd28ee3453b9b95cec6758383b946
gary <>
2023-03-08 16:56:17 +0800

    no message

--- travel_insurances.sol
+++ travel_insurances.sol
@@ -2,56 +2,111 @@
 pragma solidity ^0.8.19;
 
 contract TravelInsuranceFactory {
-    address public manager;
-    address[] public deployedInsurance;
-
-    enum InsuranceType{ PlanA, PlanB, PlanC }
+    address public manager; // our company's wallet address
+    address[] public deployedInsurances;
 
     constructor() {
         manager = msg.sender;
     }
 
-    function createTravelInsurance(InsuranceType insuranceType) public {
+    function createTravelInsurance(
+        address _insured,
+        uint256 _tripStart,
+        uint256 _tripEnd,
+        uint256 _premium,
+        uint256 _payoutAmount
+        ) public {
         // create new contract
         // add to deployedInsurance
 
-        TravelInsurance newInsurance = new TravelInsurance();
-        deployedInsurance.push(newInsurance);
+        TravelInsurance newInsurance = new TravelInsurance(
+            manager,
+            _insured,
+            _tripStart,
+            _tripEnd,
+            _premium,
+            _payoutAmount
+        );
+        deployedInsurances.push(newInsurance);
     }
 
     function getDeployedInsurance() public view returns (address[] memory) {
-        return deployedInsurance;
+        return deployedInsurances;
     }
 }
 
 
 contract TravelInsurance {
-    address public manager;
-    address public purchaser;
+    address public insurer; // our company's wallet address
+    address public insured; // the person who buys the insurance
+    uint256 public tripStart;
+    uint256 public tripEnd;
+    uint256 public premium; // price of the insurance
+    uint256 public payoutAmount; // amount to be paid out to the purchaser once claimed
+    bool public isActive;
+    bool public isPaidOut;
 
-    constructor(address manager, address purchaser) {
-        manager = manager;
-        purchaser = purchaser;
+    constructor(
+        address _insurer,
+        address _insured,
+        uint256 _tripStart,
+        uint256 _tripEnd,
+        uint256 _premium,
+        uint256 _payoutAmount
+        ) {
+        insurer = _insurer;
+        insured = _insured;
+        tripStart = _tripStart;
+        tripEnd = _tripEnd;
+        premium = _premium;
+        payoutAmount = _payoutAmount;
+        isActive = false;
+        isPaidOut = false;
     }
 
-    function pickWinner() public restricted {
-        // require(msg.sender == manager); // no need to use if use restricted
-
-        uint index = random() % players.length;
-        players[index].transfer(this.balance); // this = instance of the current contract, balance = money of the currency contract
-        players = new address[](0); //dynamic array with initial size of 0
+    function payPremium() public payable {
+        require(msg.value == premium); // Pay the premium of the insurance
+        isActive = true; // onle when the premium is paid, the insurance is active
     }
 
-    // function returnEntries() {
-    //     require(msg.sender == manager); // repeated code
-    // }
+    function cancelInsurance() public onlyInsurer {
+        isActive = false;
+    }
 
-    modifier restricted() {
-        require(msg.sender == manager);
+    function claimInsurance() public onlyInsurer {
+        require(isActive); // can only claim when the insurance is active
+        require(block.timestamp >= tripStart); // can only claim after the trip starts
+        require(block.timestamp < tripEnd); // can only claim before the trip ends
+        require(!isPaidOut); // can only claim once
+
+        payable(purchaser).transfer(payoutAmount);
+        isPaidOut = true;
+    }
+
+    modifier onlyInsurer() {
+        require(msg.sender == insurer);
         _;
     }
 
-    function getPlayers() public view returns (address[]) {
-        return players;
+    function getDetails() public view returns (
+        address _insurer,
+        address _insured,
+        uint256 _tripStart,
+        uint256 _tripEnd,
+        uint256 _premium,
+        uint256 _payoutAmount,
+        bool _isActive,
+        bool _isPaidOut
+    ) {
+        return (
+            insurer,
+            insured,
+            tripStart,
+            tripEnd,
+            premium,
+            payoutAmount,
+            isActive,
+            isPaidOut
+        );
     }
 }
